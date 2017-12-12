95653c00fed8b398482b95192db00f009eb35f24
Michael Rubanov <mrubanov@gmail.com>
2017-12-13 00:56:49 +0200

    some base

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('test');
+const bitcoin = require('bitcoin')
+const bitcoinlib = require('bitcoinjs-lib')
+
+console.info('Exporting `module.exports`.');
+
+module.exports = {
+  bitcoin,
+  bitcoinlib
+}
